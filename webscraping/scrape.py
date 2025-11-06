e0bd8092b6451ebd140e451821754364808a5bf0
YashChavan999 <yaash.chavan@gmail.com>
2025-11-05 21:47:09 -0500

    Test github workflow

---
+++ webscraping/scrape.py
@@ -0,0 +1,24 @@
+from product_scraper import ProductScraper
+from product_scraper import FlowerCategory
+
+import csv
+
+
+scraper = ProductScraper()
+# data = (scraper.scrape_products(FlowerCategory.DAILY_DEALS) +
+#         scraper.scrape_products(FlowerCategory.ROSE) +
+#         scraper.scrape_products(FlowerCategory.SUNFLOWER)
+# )
+
+data = scraper.scrape_products(FlowerCategory.DAILY_DEALS)
+
+fieldnames = ["Identifier", "Cost", "Type", "Color", "Number of Flowers per Package", "Stem Length", "Shipping Time (Hours)"]
+output_filename = "products.csv"
+
+with open(f"../data/{output_filename}", 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(data)
+
+print(f"Data successfully written to {output_filename}")
