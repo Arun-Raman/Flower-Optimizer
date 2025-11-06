23e465af1cfb46b799f5c6db77eb1731bde04085
YashChavan999 <yaash.chavan@gmail.com>
2025-11-05 22:03:19 -0500

    Write data to "/data" instead of root

--- webscraping/scrape.py
+++ webscraping/scrape.py
@@ -1,3 +1,5 @@
+import os
+
 from product_scraper import ProductScraper
 from product_scraper import FlowerCategory
 
@@ -14,9 +16,10 @@ data = scraper.scrape_products(FlowerCategory.DAILY_DEALS)
 
 fieldnames = ["Identifier", "Cost", "Type", "Color", "Number of Flowers per Package", "Stem Length", "Shipping Time (Hours)"]
 
+output_dir = "data"
 output_filename = "products.csv"
 
-with open(output_filename, 'w', newline='') as csvfile:
+with open(os.path.join(output_dir, output_filename), 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
